3be6cecd242b64693f58c6eac8914d051c496e9a
baligashreya <shreyabaliga248@gmail.com>
2020-04-10 15:15:18 +0400

    Updates

---
+++ precache-manifest.08f6b485332d0b3e80b8269d8611a5a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08738345c8a707eb94946a8520827cc",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "308c0e4186958b1db137",
+    "url": "/covid19-tracker/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "747eb7fe0b045f5a6eca",
+    "url": "/covid19-tracker/static/css/main.78038d93.chunk.css"
+  },
+  {
+    "revision": "308c0e4186958b1db137",
+    "url": "/covid19-tracker/static/js/2.7bccf9e5.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/covid19-tracker/static/js/2.7bccf9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747eb7fe0b045f5a6eca",
+    "url": "/covid19-tracker/static/js/main.e907eb73.chunk.js"
+  },
+  {
+    "revision": "d625124cf7b49aa5036c",
+    "url": "/covid19-tracker/static/js/runtime-main.cd6b3170.js"
+  }
+]);
